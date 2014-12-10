6d4940c90ccfe2e570cf8746bdf8fdbccbc836b7
Kevin Kwon <kvnkwon@gmail.com>
2014-12-10 16:42:10 -0500

    updated callback route

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,7 +3,7 @@ get '/login' do
   redirect("/auth/github")
 end
 
-get '/auth/callback' do
+get '/auth/github/callback' do
   auth = request.env["omniauth.auth"]
   user = User.find_or_create_by({
     uid: auth.uid,
