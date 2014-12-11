e8d64da0f65878a8522c749de18af8a7d5dc8391
Kevin Kwon <kvnkwon@gmail.com>
2014-12-11 15:16:48 -0500

    took out the js

--- public/js/application.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-  $(".success-prompt").ready(function(){
-    window.setTimeout(function(){
-      alert("within the timeout function")
-    window.location.href='/';
-    }, 3000);
-  })
-});
