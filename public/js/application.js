d65f0d0a578b3b4c80a1cf816cf1c787a5b6aa66
Kevin Kwon <kvnkwon@gmail.com>
2014-12-11 14:15:53 -0500

    implementing js redirect

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,9 @@
-application.js
+$(document).ready(function () {
+
+  $(".success-prompt").on("load", function(){
+    window.setTimeout(function(){
+      alert("within the timeout function")
+    window.location.href='/';
+    , 3000);
+  })
+});
