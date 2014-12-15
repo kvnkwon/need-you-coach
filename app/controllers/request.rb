76192535f762646308fe4c3e3423ff6fa5a0dfac
Kevin Kwon <kvnkwon@gmail.com>
2014-12-15 14:40:56 -0500

    converted button to reg link

--- app/controllers/request.rb
+++ app/controllers/request.rb
@@ -33,6 +33,7 @@ post '/request/:id' do
   user_request = Request.find_by(id: params[:id])
   if current_user.is_admin?
     user_request.update(coach_id: current_user.id)
+    redirect('/')
   else
     redirect('/')
   end
