63156d0c27b89b3c2f5dafb1725c57d0447b36c4
Kevin Kwon <kvnkwon@gmail.com>
2014-12-16 11:18:23 -0500

    testing more json

--- public/js/application.js
+++ public/js/application.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $('.help-form').submit(function(event) {
     event.preventDefault();
     var $target = $(event.target);
-    console.log($target)
     $.ajax({
       type: "POST",
-      url: $target.attr("action")
+      url: $target.attr("action"),
+      dataType: "JSON"
     }).done(function(response) {
       console.log(response)
       $target.html('- Coach <a href="/user/' + response.id + '">' + response.name + '</a> is helping.')
