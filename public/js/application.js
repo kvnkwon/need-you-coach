8f78905faecd3a9ca5873f1243cbb620616e95ae
Kevin Kwon <kvnkwon@gmail.com>
2014-12-16 11:06:13 -0500

    change to coach json

--- public/js/application.js
+++ public/js/application.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
   $('.help-form').submit(function(event) {
     event.preventDefault();
     var $target = $(event.target);
-
+    console.log($target)
     $.ajax({
       type: "POST",
       url: $target.attr("action")
     }).done(function(response) {
       console.log(response)
+      $target.html('- Coach <a href="/user/' + response.id + '">' + response.name + '</a> is helping.')
     })
   })
 })
