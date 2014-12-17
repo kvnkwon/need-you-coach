28722527bb83035f1ff9bef7d540361e90745627
Kevin Kwon <kvnkwon@gmail.com>
2014-12-17 15:04:07 -0500

    added all coaches to coach show page

--- app/controllers/coach.rb
+++ app/controllers/coach.rb
@@ -1,5 +1,6 @@
 # View all coaches who are checked in
 get '/coaches' do
   @coaches = User.where(checked_in: true)
+  @all_coaches = User.where(is_admin: true)
   erb :'coach/show'
 end
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,5 @@
 get '/' do
   @user_requests = Request.all.order('created_at desc')
-  @help_needed_requests = Request.where(coach_id: nil)
   @coaches = User.where(checked_in: true)
   erb :index
 end
